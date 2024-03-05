315deec29069f2eb29df40ffc8e105bc589167be
ranaadeel15911 <ra0511083@gmail.com>
2024-03-06 02:04:40 +0500

    ajfa

--- sanity/env.js
+++ sanity/env.js
@@ -1,6 +1,5 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-29'
+export const apiVersion = '2023-10-29'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = "production";
+export const projectId = "w0b9yzct";
 export const useCdn = false
--- sanity/lib/image.js
+++ sanity/lib/image.js
@@ -3,8 +3,8 @@ import createImageUrlBuilder from '@sanity/image-url'
 import { dataset, projectId } from '../env'
 
 const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
+  projectId: "w0b9yzct" || '',
+  dataset: "production" || '',
 })
 
 export const urlForImage = (source) => {
